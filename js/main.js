2418a6a9e5da8d8815141c0bf99aca3ac97c63d7
Lena M. <lena.miyamoto21@gmail.com>
2014-07-21 01:39:56 +0200

    Added base for shift mechanism

--- js/main.js
+++ js/main.js
@@ -6,7 +6,18 @@
 		this.entrance = entrance === undefined ? -1 : entrance % 4; // spawn point for placer n (0-3)
 		this.treasure = -1; // default value: -1 (no treasure)
 		this.$container = null;
+		this.x = -1;
+		this.y = -1;
 	};
+	Tile.prototype.addShiftListener = function(listener) {
+		var self = this;
+		
+		this.listener = listener;
+		this.$container.click(function(){
+			self.listener.shift(self.x, self.y);
+			console.log('Move tile to ' + self.x + '/' + self.y);
+		});
+	};	
 	Tile.prototype.setTreasure = function(treasure) {
 		this.treasure = treasure;
 	};
@@ -18,7 +29,7 @@
 			classes += ' fixed';
 		}
 		if (this.entrance != -1) {
-			var $entrance = $('<div/>').attr('class', 'entrance ' + Tile.getPlayerClassName(this.entrance));
+			var $entrance = $('<div/>').addClass('entrance ' + Tile.getPlayerClassName(this.entrance));
 			$container.append($entrance);
 		}		
 		$container.attr('class', classes);
@@ -27,7 +38,45 @@
 		var xPos = Tile.SIZE_IN_PX + Tile.SIZE_IN_PX * x;
 		var yPos = Tile.SIZE_IN_PX + Tile.SIZE_IN_PX * y;
 		
+		this.x = x;
+		this.y = y;
 		this.$container.css({'top' : yPos + 'px', 'left' : xPos + 'px'});
+	};
+	/**
+	 * Animate movement.
+	 * 
+	 * @param x coordinate X
+	 * @param y coordiante Y
+	 * @see setPosition
+	*/
+	Tile.prototype.moveTo = function(x, y) {
+		var xPos = Tile.SIZE_IN_PX + Tile.SIZE_IN_PX * x;
+		var yPos = Tile.SIZE_IN_PX + Tile.SIZE_IN_PX * y;
+		
+		this.x = x;
+		this.y = y;
+		this.$container.css({'top' : yPos + 'px', 'left' : xPos + 'px'});
+		// TODO add CSS animations
+	};
+	Tile.prototype.rotate = function() {
+		switch (orientation) {
+			case Tile.ORIENTATION.NORTH:
+				this.$container.removeClass('north').addClass('east');
+				this.orientation = Tile.ORIENTATION.EAST;
+				break;
+			case Tile.ORIENTATION.EAST:
+				this.$container.removeClass('east').addClass('south');
+				this.orientation = Tile.ORIENTATION.SOUTH;
+				break;
+			case Tile.ORIENTATION.SOUTH:
+				this.$container.removeClass('south').addClass('west');
+				this.orientation = Tile.ORIENTATION.WEST;
+				break;
+			case Tile.ORIENTATION.WEST:
+				this.$container.removeClass('west').addClass('north');
+				this.orientation = Tile.ORIENTATION.NORTH;
+				break;
+		}
 	};
 	Tile.getTypeClassName = function(type) {
 		switch (type) {
@@ -116,6 +165,7 @@
 		this.freeTile = null;
 		
 		this.init = function() {
+			var self = this;
 			var type, orientation;
 			var corners   = CrazyMaze.CORNERS;
 			var straights = CrazyMaze.STRAIGHTS;
@@ -173,23 +223,98 @@
 					tile.createContainer();
 					$maze.append(tile.$container);
 					tile.setPosition(col, row);
+					tile.addShiftListener(this);
 				}
 			}
 			
 			this.freeTile.createContainer();
 			$maze.append(this.freeTile.$container);
 			this.freeTile.setPosition(1, 7);
+			this.freeTile.addShiftListener(this);
+		};
+		
+		/**
+		 * ShiftListener interface.
+		*/
+		this.shift = function(x, y) {
+			if (x == 0) {
+				this.shiftEast(x, y, this.freeTile);
+			} else if (x == CrazyMaze.SIZE - 1) {
+				this.shiftWest(x, y, this.freeTile);
+			} else if (y == 0) {
+				this.shiftSouth(x, y, this.freeTile);
+			} else if (y == CrazyMaze.SIZE - 1) {
+				this.shiftNorth(x, y, this.freeTile);
+			}
+			// else: ignore move
+		};
+		
+		this.shiftNorth = function(x, y, tile) {
+			// TODO check range
+			var tmpTile, currTile = tile;
+			
+			for (var row = y; row > 0; --row) {
+				currTile.moveTo(x, row);
+				
+				tmpTile = maze[row][x];
+				maze[row][x] = currTile;
+				currTile = tmpTile;
+			}
+			this.freeTile = currTile;
+		};
+		
+		this.shiftSouth = function(x, y, tile) {
+			// TODO check range
+			var tmpTile, currTile = tile;
+			
+			for (var row = y; row < CrazyMaze.SIZE; ++row) {
+				currTile.moveTo(x, row);
+				
+				tmpTile = maze[row][x];
+				maze[row][x] = currTile;
+				currTile = tmpTile;
+			}
+			this.freeTile = currTile;
+		};
+		
+		this.shiftEast = function(x, y, tile) {
+			// TODO check range
+			var tmpTile, currTile = tile;
+			
+			for (var col = x; col > 0; --col) {
+				currTile.moveTo(col, y);
+				
+				tmpTile = maze[y][col];
+				maze[y][col] = currTile;
+				currTile = tmpTile;
+			}
+			this.freeTile = currTile;
+		};
+		
+		this.shiftWest = function(x, y, tile) {
+			// TODO check range
+			var tmpTile, currTile = tile;
+			
+			for (var col = x; col < CrazyMaze.SIZE; ++col) {
+				currTile.moveTo(col, y);
+				
+				tmpTile = maze[y][col];
+				maze[y][col] = currTile;
+				currTile = tmpTile;
+			}
+			this.freeTile = currTile;
 		};
 	};
 	CrazyMaze.CROSSINGS = 6;
 	CrazyMaze.CORNERS = 15;
 	CrazyMaze.STRAIGHTS = 13;
+	CrazyMaze.SIZE = 7;
 	
 	$(function(){
-		var game = new CrazyMaze();
+		window.game = new CrazyMaze();
 		
-		game.init();
-		game.render();
+		window.game.init();
+		window.game.render();
 	});
 })();
 
